22bf53c85219c58b6d0a68aceba1c084ca5b3525
[Hameed Asmath] <75921618+HameedAsmath@users.noreply.github.com>
2023-01-29 14:06:56 +0530

    Minor Bug Fixes

--- src/views/CarouselContent.jsx
+++ src/views/CarouselContent.jsx
@@ -1,16 +1,6 @@
-import React, { lazy } from "react";
-// import { link45, file, check2all } from "../npm/icon";
+import React from "react";
 import { data } from "../data";
 
-const Support = lazy(() => import("../components/Support"));
-const Banner = lazy(() => import("../components/carousel/Banner"));
-const Carousel = lazy(() => import("../components/carousel/Carousel"));
-const CardIcon = lazy(() => import("../components/card/CardIcon"));
-const CardLogin = lazy(() => import("../components/card/CardLogin"));
-const CardImage = lazy(() => import("../components/card/CardImage"));
-const CardDealsOfTheDay = lazy(() =>
-  import("../components/card/CardDealsOfTheDay")
-);
 
 
 const CarouselContent = () => {
@@ -24,21 +14,6 @@ const CarouselContent = () => {
     productRows.map((row, idx) => (
       <div className={`carousel-item ${idx === 0 ? "active" : ""}`} key={idx}>
         <div className="row g-3">
-          {/* {row.map((product, idx) => {
-            const ProductImage = components[prCarouselContentoduct.img];
-            return (
-              <div key={idx} className="col-md-3">
-                <CardIcon
-                  title={product.title}
-                  text={product.text}
-                  tips={product.tips}
-                  to={product.to}
-                >
-                  <ProductImage className={product.cssClass} width="80" height="80" />
-                </CardIcon>
-              </div>
-            );
-          })} */}
         </div>
       </div>
     ))
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,95 +1,26 @@
 import React, { lazy, Component } from "react";
-import { Link } from "react-router-dom";
-// import { link45, file, check2all } from "../npm/icon";
 import { data } from "../data";
-import CarouselContent from "./CarouselContent";
 
 const Support = lazy(() => import("../components/Support"));
 const Banner = lazy(() => import("../components/carousel/Banner"));
 
-const Home = () => {
-  return (
-    <>
-      <Banner className="mb-3" id="carouselHomeBanner" data={data.banner} />
-      <div className="container-fluid bg-light mb-3">
-        <div className="row g-3">
-          <div className="col-md-9">
-            <Carousel id="elect-product-category" className="mb-3">
-              {CarouselContent}
-            </Carousel>
-            <Support />
-          </div>
-          <div className="col-md-3">
-            <CardLogin className="mb-3" />
-            <CardImage src="../../images/banner/Watches.webp" to="promo" />
+class HomeView extends Component {
+  render() {
+    return (
+      <>
+        <Banner className="mb-3" id="carouselHomeBanner" data={data.banner} />
+        <div className="container-fluid bg-light mb-3">
+          <div className="row g-3">
+            <div className="col-6 col-md-12 px-4">
+              <Support />
+            </div>
+           
           </div>
         </div>
-      </div>
-      <div className="container-fluid bg-light mb-3">
-        <div className="row">
-          <div className="col-md-12">
-            <CardDealsOfTheDay
-              endDate={Date.now() + 1000 * 60 * 60 * 14}
-              title="Deals of the Day"
-              to="/"
-            >
-              <Carousel id="elect-product-category1">
-                {CarouselContent}
-              </Carousel>
-            </CardDealsOfTheDay>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-info bg-gradient p-3 text-center mb-3">
-        <h4 className="m-0">Explore Fashion Collection</h4>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">
-            <Link to="/" className="text-decoration-none">
-              <img
-                src="../../images/category/male.webp"
-                className="img-fluid rounded-circle"
-                alt="..."
-              />
-              <div className="text-center h6">Men's Clothing</div>
-            </Link>
-          </div>
-          <div className="col-md-3">
-            <Link to="/" className="text-decoration-none">
-              <img
-                src="../../images/category/female.webp"
-                className="img-fluid rounded-circle"
-                alt="..."
-              />
-              <div className="text-center h6">Women's Clothing</div>
-            </Link>
-          </div>
-          <div className="col-md-3">
-            <Link to="/" className="text-decoration-none">
-              <img
-                src="../../images/category/smartwatch.webp"
-                className="img-fluid rounded-circle"
-                alt="..."
-              />
-              <div className="text-center h6">Smartwatch</div>
-            </Link>
-          </div>
-          <div className="col-md-3">
-            <Link to="/" className="text-decoration-none">
-              <img
-                src="../../images/category/footwear.webp"
-                className="img-fluid rounded-circle"
-                alt="..."
-              />
-              <div className="text-center h6">Footwear</div>
-            </Link>
-          </div>
-        </div>
-      </div>
+      
     </>
   )
 }
+}
 
-export default Home
+export default HomeView
