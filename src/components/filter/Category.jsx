6ee597b07248f1d9fd68f87f662dfcd170ce9612
[Hameed Asmath] <75921618+HameedAsmath@users.noreply.github.com>
2023-01-29 14:39:45 +0530

    Minor changes

--- src/components/filter/Category.jsx
+++ src/components/filter/Category.jsx
@@ -19,32 +19,22 @@ const FilterCategory = (props) => {
       >
         <li className="list-group-item">
           <Link to="/" className="text-decoration-none stretched-link">
-            Clothing
+            Fruit
           </Link>
         </li>
         <li className="list-group-item">
           <Link to="/" className="text-decoration-none stretched-link">
-            Leather Bag
+             Pulse
           </Link>
         </li>
         <li className="list-group-item">
           <Link to="/" className="text-decoration-none stretched-link">
-            Trausers
+            Rice
           </Link>
         </li>
         <li className="list-group-item">
           <Link to="/" className="text-decoration-none stretched-link">
-            Sweater & Cardigans
-          </Link>
-        </li>
-        <li className="list-group-item">
-          <Link to="/" className="text-decoration-none stretched-link">
-            High Heels
-          </Link>
-        </li>
-        <li className="list-group-item">
-          <Link to="/" className="text-decoration-none stretched-link">
-            Coats & Jackets
+            Vegetables
           </Link>
         </li>
       </ul>
