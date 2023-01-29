d5afec7b7525a5debdec5f82049642d9a3570f57
[Hameed Asmath] <75921618+HameedAsmath@users.noreply.github.com>
2023-01-29 13:49:09 +0530

    Homepage UI completed

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { ReactComponent as IconCash } from "bootstrap-icons/icons/cash.svg";
 import { ReactComponent as IconHeadset } from "bootstrap-icons/icons/headset.svg";
 import { ReactComponent as IconTruck } from "bootstrap-icons/icons/truck.svg";
+import { ReactComponent as IconDaily } from "bootstrap-icons/icons/calendar2.svg";
+
 const Support = (props) => {
   return (
     <div className={`row g-3 ${props.className}`}>
-      <div className="col-md-4">
+      <div className="col-md-3">
         <div className="card bg-primary">
           <div className="card-body text-white">
             <span className="p-3 bg-light rounded-circle me-3 text-dark">
@@ -15,7 +17,7 @@ const Support = (props) => {
           </div>
         </div>
       </div>
-      <div className="col-md-4">
+      <div className="col-md-3">
         <div className="card bg-danger">
           <div className="card-body text-white">
             <span className="p-3 bg-light rounded-circle me-3 text-dark">
@@ -25,7 +27,17 @@ const Support = (props) => {
           </div>
         </div>
       </div>
-      <div className="col-md-4">
+      <div className="col-md-3">
+        <div className="card bg-warning">
+          <div className="card-body text-white">
+            <span className="p-3 bg-light rounded-circle me-3 text-dark">
+              <IconDaily width={40} height={40} />
+            </span>
+            Daily Products
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
         <div className="card bg-success">
           <div className="card-body text-white">
             <span className="p-3 bg-light rounded-circle me-3 text-dark">
--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import axios from "axios"
+import {useState} from "react"
 
 const TopMenu = () => {
+  const[categories,setCategories] = useState([])
+  const fetchData = async()=>{
+    const res =await axios.get("https://api.rajeshwaran.me/api/category/")
+    setCategories(res.data)
+    console.log(res.data)
+  }
+  fetchData()
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
+    <nav className="navbar navbar-expand-lg navbar-primary bg-primary px-3">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          E-Commerce
+        <Link className="navbar-brand text-light" to="/">
+          <b>E-Commerce</b>
         </Link>
         <button
           className="navbar-toggler"
@@ -18,102 +27,15 @@ const TopMenu = () => {
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse mr-0" id="navbarSupportedContent">
           <ul className="navbar-nav">
-            <li className="nav-item dropdown">
-              <button
-                className="btn nav-link dropdown-toggle fw-bold"
-                id="navbarDropdown"
-                data-toggle="dropdown"
-                aria-expanded="false"
-                data-bs-toggle="dropdown"
-              >
-                All Pages
-              </button>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <Link className="dropdown-item" to="/account/signin">
-                    Sign In
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/account/signup">
-                    Sign Up
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/checkout">
-                    Checkout Page
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/contact-us">
-                    Contact Us
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/blog">
-                    Blog
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/blog/detail">
-                    Blog Detail
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/fsafasf">
-                    404 Page Not Found
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/500">
-                    500 Internal Server Error
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Fashion
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Supermarket
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Electronics
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Furniture
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Garden & Outdoors
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/category">
-                Jewellery
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/documentation">
-                Documentation
-              </Link>
-            </li>
+            {categories.map((category)=>(
+                <li className="nav-item lead" key={category.id}>
+                <Link className="nav-link text-light" to="/category">
+                  <b>{category.name}</b>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
